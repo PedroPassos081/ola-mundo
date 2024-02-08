0ed5d263b8cdf93676de90be44e5d9b3c137f3ac
PedroPassos081 <pedro.passos081@gmail.com>
2024-02-08 13:38:27 -0300

    Implementando pequenas mudanças no "sobre mim"

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -25,11 +25,12 @@ function SobreMin() {
           Oi, tudo bem? Eu sou estudante de análise e densevolvimento de sistemas na UNIFG e estou feliz de te ver por aqui.
       </p>
       <p className={styles.paragrafo}>
-          Minha história com programação começou em 2022 quando ingressei no meu curso(ADS). Onde eu aprendi lógica de programação e o básico de várias linguagens, como Python, Java. E logo me apaixonei pela programação.
+          Minha história com programação começou em 2022 quando ingressei no meu curso(ADS). Onde eu aprendi lógica de programação e o básico de várias linguagens, como Python, Java. E logo me apaixonei pela programação, e sabia que era isso que queria para o meu futuro.
       </p>
       
       <p className={styles.paragrafo}>
-          Com isso resolvi me aprofundar em estudar a parte do front-end, onde comecei com o HTML,CSS e Javascript, as tecnologias que mais uso no meu dia dia são, Javascript, React e typescript
+          Com isso resolvi me aprofundar em estudar para ser um programador front-end, onde comecei com o HTML, CSS e Javascript, bootstrap, Sass e logo em seguida me aprofundei em alguns frameworks como React.js, node e typescript.
+            Sou um desenvolvedor apaixonado por tecnologia e programação.
       </p>
       <p className={styles.paragrafo}>
         Desde então, tem sido aprendizados atrás de aprendizados.
