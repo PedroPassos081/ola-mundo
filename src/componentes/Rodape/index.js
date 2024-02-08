5fcbfb4fc296dafb21cc41538cb170d74dac10c8
PedroPassos081 <pedro.passos081@gmail.com>
2024-02-08 13:04:53 -0300

    Finalizando projeto

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -7,7 +7,7 @@ function  Rodape() {
         <footer className={styles.rodape}>
             <MarcaRegistrada />
 
-            Desenvolvido por Alura.
+            Desenvolvido por mim e Alura.
         </footer>
     )
 
